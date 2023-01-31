b3ecfc41b21aa6aa559abde814b603ef108ef8c8
ben lim <lim6112j@gmail.com>
2023-01-31 16:51:11 +0900

    ...

--- src/clojure_noob/ch3/exercise.clj
+++ src/clojure_noob/ch3/exercise.clj
@@ -1,10 +1,33 @@
 (ns clojure-noob.ch3.exercise)
 (defn hello []
   (println "hello"))
-(def aList '(1 2 3))
-(def aVector [1 2 3])
+; map
+{} ; empty map
+{:first-name "ben" :last-name "lim"}
+{"string-key" +} ; string-key to + function
+; using hash-map function to make a map
 (def aHashMap (hash-map :a 1 :b 2))
 (def aHashSet (hash-set :a :b :a))
+(get aHashMap :a) ; get :a value from aHashMap
+(get {:a 0 :b 1} :c "unicorns?")  ; default value
+(get-in {:a 0 :b {:c "ho hum"}} [:b :c]) ; lookup in nested maps
+({:a 1 :b 2} :a) ; map is function-like
+; keywords
+(:a {:a 1 :b 2}) ; == (get {:a 1 :b 2} :a),  keywords like function
+; vectors
+[1 2 3]
+(def aVector [1 2 3])
+(get [1 2 3] 0) ; 0 idx value is 1
+(vector 1 2 3) ; vector function creates vector
+(conj [1 2 3] 4)
+; => [1 2 3 4] 
+
+; lists
+'(1 3 4) 
+; => (1 2 3) 
+(def aList '(1 2 3))
+
+
 (defn adds100
   "adds 100 function"
   [x]
