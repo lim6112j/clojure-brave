ddc32ca624dd079acd09c81514b0950419f9638a
lim6112j <lim6112j@gmail.com>
2023-04-16 11:16:36 +0900

    ch9 ing

--- src/clojure_noob/ch9/exercise.clj
+++ src/clojure_noob/ch9/exercise.clj
@@ -7,3 +7,23 @@
   (println "deref: " (deref result))
   (println "@: " @result))
 (deref (future (Thread/sleep 1000) 0) 10 5)
+(def jackson-5-delay
+  (delay (let [message "Just call my name and I'll be there"]
+           (println "First deref: " message)
+           message)))
+(force jackson-5-delay)
+(def gimli-headshots ["serious.jpg" "fun.jpg" "playfun.jsp"])
+(defn email-user
+  [email-address]
+  (println "Sending headshot nofification to" email-address))
+(defn upload-document
+  "Needs to be implemented"
+  [headshot]
+  true)
+(let [notify (delay (email-user "and-my-axe@gmail.com"))]
+  (doseq [headshot gimli-headshots]
+    (future (upload-document headshot)
+            (force notify))))
+(def my-promise (promise))
+(deliver my-promise (+ 1 2))
+(println "my-promise : " @my-promise)
