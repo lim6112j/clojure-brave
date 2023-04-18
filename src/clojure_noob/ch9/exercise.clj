0707eb50cf99dde5ae96da95b41639433b20af64
ben lim <lim6112j@gmail.com>
2023-04-18 16:41:05 +0900

    more example

--- src/clojure_noob/ch9/exercise.clj
+++ src/clojure_noob/ch9/exercise.clj
@@ -1,29 +1,52 @@
 (ns clojure-noob.ch9.exercise)
-(future (Thread/sleep 4000)
-        (println "I'll print after 4 seconds"))
-(println "I'll print immediately")
-(let [result (future (println "this prints once")
-                     (+ 1 1))]
-  (println "deref: " (deref result))
-  (println "@: " @result))
-(deref (future (Thread/sleep 1000) 0) 10 5)
-(def jackson-5-delay
-  (delay (let [message "Just call my name and I'll be there"]
-           (println "First deref: " message)
-           message)))
-(force jackson-5-delay)
-(def gimli-headshots ["serious.jpg" "fun.jpg" "playfun.jsp"])
-(defn email-user
-  [email-address]
-  (println "Sending headshot nofification to" email-address))
-(defn upload-document
-  "Needs to be implemented"
-  [headshot]
-  true)
-(let [notify (delay (email-user "and-my-axe@gmail.com"))]
-  (doseq [headshot gimli-headshots]
-    (future (upload-document headshot)
-            (force notify))))
-(def my-promise (promise))
-(deliver my-promise (+ 1 2))
-(println "my-promise : " @my-promise)
+; (future (Thread/sleep 4000)
+;         (println "I'll print after 4 seconds"))
+; (println "I'll print immediately")
+; (let [result (future (println "this prints once")
+;                      (+ 1 1))]
+;   (println "deref: " (deref result))
+;   (println "@: " @result))
+; (deref (future (Thread/sleep 1000) 0) 10 5)
+; (def jackson-5-delay
+;   (delay (let [message "Just call my name and I'll be there"]
+;            (println "First deref: " message)
+;            message)))
+; (force jackson-5-delay)
+; (def gimli-headshots ["serious.jpg" "fun.jpg" "playfun.jsp"])
+; (defn email-user
+;   [email-address]
+;   (println "Sending headshot nofification to" email-address))
+; (defn upload-document
+;   "Needs to be implemented"
+;   [headshot]
+;   true)
+; (let [notify (delay (email-user "and-my-axe@gmail.com"))]
+;   (doseq [headshot gimli-headshots]
+;     (future (upload-document headshot)
+;             (force notify))))
+; (def my-promise (promise))
+; (deliver my-promise (+ 1 2))
+; (println "my-promise : " @my-promise)
+; yak-butter
+(def yak-butter-international
+  {:store "Yak Butter International"
+   :price 90
+   :smoothness 90})
+(def butter-than-nothing
+  {:store "Butter thank Nothing"
+   :price 150
+   :smoothness 83})
+(def baby-got-yak
+  {:store "Baby got Yak"
+   :price 94
+   :smoothness 99})
+(defn mock-api-call
+  [result]
+  (Thread/sleep 1000)
+  result)
+(defn satisfactory?
+  "if the butter meets our criteria, return the butter, else return "
+  [butter]
+  (and (<= (:price butter) 100)
+       (>= (:smoothness butter) 97)
+       butter))
--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,23 +1,23 @@
 (ns clojure-noob.core
   (:gen-class)
   (:require
-   [clojure-noob.ch4.exercise :refer [titleize]]
-   [clojure-noob.ch5.exercise :refer [year-end-evaluation sum]]
-   [clojure-noob.ch11.exercise]
+   ; [clojure-noob.ch4.exercise :refer [titleize]]
+   ; [clojure-noob.ch5.exercise :refer [year-end-evaluation sum]]
+   ; [clojure-noob.ch11.exercise]
    [clojure-noob.ch9.exercise]))
 (use 'clojure-noob.ch3.exercise)
 (def dec9 (dec-maker 9))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (hello)
-  (println "a List : " aList)
-  (println "a Vector : " aVector)
-  (println "a hash-map" aHashMap)
-  (println "a hash-set" aHashSet)
-  (println "adds 100 3" (adds100 3))
-  (println "dec 9 from 10" (dec9 10))
-  (println "mapset inc [1, 1, 2,2]" (mapset inc [1 1 2 2]))
-  (println "titleize : " (map titleize '("Empathy", "Decoration") ))
-  (year-end-evaluation)
+  ; (hello)
+  ; (println "a List : " aList)
+  ; (println "a Vector : " aVector)
+  ; (println "a hash-map" aHashMap)
+  ; (println "a hash-set" aHashSet)
+  ; (println "adds 100 3" (adds100 3))
+  ; (println "dec 9 from 10" (dec9 10))
+  ; (println "mapset inc [1, 1, 2,2]" (mapset inc [1 1 2 2]))
+  ; (println "titleize : " (map titleize '("Empathy", "Decoration") ))
+  ; (year-end-evaluation)
   )
