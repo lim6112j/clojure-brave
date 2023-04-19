cf3bd089beef0674bdfdeec78de8fd4902adea1b
lim6112j <lim6112j@gmail.com>
2023-04-19 22:17:24 +0900

    more macro

--- src/clojure_noob/ch8/exercise.clj
+++ src/clojure_noob/ch8/exercise.clj
@@ -21,17 +21,35 @@
   (list 'let ['result expression]
         (list 'println 'result)
         'result))
-(println (list '+ 1 (inc 1))) ; normal quote
-(println `(+ 1 ~(inc 1))) ;syntax quote
+; (println (list '+ 1 (inc 1))) ; normal quote
+; (println `(+ 1 ~(inc 1))) ;syntax quote
+; (defmacro code-critic
+;   "Pharases are courtesy hermes conrand from futurama"
+;   [bad good]
+;   (list 'do
+;         (list 'println
+;               "Greate sqid of madrid, this is bad code:"
+;               (list 'quote bad))
+;         (list 'println
+;               "Sweet gorilla of Manila, this is good code!"
+;               (list 'quote good))))
+; (code-critic #_{:clj-kondo/ignore [:not-a-function]}
+;  (1 + 1) (+ 1 1))
+(defn criticize-code
+  [criticism code]
+  `(println ~criticism (quote ~code)))
 (defmacro code-critic
-  "Pharases are courtesy hermes conrand from futurama"
   [bad good]
-  (list 'do
-        (list 'println
-              "Greate sqid of madrid, this is bad code:"
-              (list 'quote bad))
-        (list 'println
-              "Sweet gorilla of Manila, this is good code!"
-              (list 'quote good))))
-(code-critic #_{:clj-kondo/ignore [:not-a-function]}
- (1 + 1) (+ 1 1))
+  `(do ~@(map #(apply criticize-code %)
+              [["Sweet lion of zion :" bad]
+               ["Great cow of Moscow:" good]])))
+; (code-critic #_{:clj-kondo/ignore [:not-a-function]}
+;  (1 + 1) (+ 1 1))
+
+(def message "Good Job")
+(defmacro with-mischief
+  [& stuff-to-do]
+  (concat (list 'let ['message "oh, big deal"])
+          stuff-to-do))
+(with-mischief
+  (println "Here's how I feel about that thing you did: " message))
