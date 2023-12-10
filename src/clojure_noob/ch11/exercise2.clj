c1a34ec05c6bc45edd8ae7ef92c0b35ce251cb02
lim6112j <lim6112j@gmail.com>
2023-12-10 09:16:12 +0900

    more

--- src/clojure_noob/ch11/exercise2.clj
+++ src/clojure_noob/ch11/exercise2.clj
@@ -1,5 +1,7 @@
 (ns clojure-noob.ch11.exercise2
-  (:require [clojure.core.async :as a]))
+  (:require
+   [clojure.core.async :as a]
+   [clojure.string :as s]))
 (defn hot-dog-machine
   []
   (let [in (a/chan)
@@ -21,6 +23,11 @@
                       (recur hc))))
               (do (a/close! in) (a/close! out)))))
     [in out]))
+(let [c1 (a/chan) c2 (a/chan) c3 (a/chan)]
+  (a/go (a/>! c2 (s/upper-case (a/<! c1))))
+  (a/go (a/>! c3 (s/reverse (a/<! c2))))
+  (a/go (println (a/<! c3)))
+  (a/>!! c1 "redrum"))
 
 (defn -main
   [& args]
