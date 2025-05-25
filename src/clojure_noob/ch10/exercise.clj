c9343c978d6b941af67ab6a898ede42858573a2f
ben <Lim6112j@gmail.com>
2025-05-25 16:50:54 +0900

    chapter10

--- src/clojure_noob/ch10/exercise.clj
+++ src/clojure_noob/ch10/exercise.clj
@@ -22,3 +22,75 @@
 ; => {:a {:b 13}}
 
 (swap! fred update-in [:cuddle-hunger-level] inc)
+
+(let [num (atom 1) s1 @num]
+  (swap! num inc)
+  (println "State 1: " s1)
+  (println "Current state: " @num))
+(defn shuffle-speed
+  [zombie]
+  (* (:cuddle-hunger-level zombie)
+     (- 100 (:percent-deteriorated zombie))))
+(defn shuffle-alert
+  [key watched old-state new-state]
+  (let [sph (shuffle-speed new-state)]
+    (if (> sph 5000)
+      (do
+        (println "Run, you fool!")
+        (println "The zombie's sph is now " sph)
+        (println "This message brought to your courtesy of " key))
+      (do
+        (println "All is well with" key)
+        (println "Cuddle hunger: " (:cuddle-hunger-level new-state))
+        (println "Percent deteriorated: " (:percent-deteriorated new-state))
+        (println "Sph: " sph)))))
+(defn percent-deteriorated-validator
+  [{:keys [percent-deteriorated]}]
+  (or (and (>= percent-deteriorated 0)
+           (<= percent-deteriorated 100))
+      (throw (IllegalStateException. "That's not matchy"))))
+(def bobby
+  (atom {:cuddle-hunger-level 0 :percent-deteriorated 0}
+        :validator percent-deteriorated-validator))
+
+(def sock-varieties
+  #{"darned" "argyle" "wool" "horsehair" "mulleted"
+    "passive-aggressive" "striped" "polka-dotted"
+    "athletic" "business" "power" "invisible" "gollumed"})
+(defn sock-count
+  [sock-variety count]
+  {:variety sock-variety
+   :count count})
+(defn generate-sock-gnome
+  "Create an initial sock gnome state with no socks"
+  [name]
+  {:name name :socks #{}})
+(defn steal-sock
+  [gnome dryer]
+  (dosync
+   (when-let [pair (some #(if (= (:count %) 2) %) (:socks @dryer))]
+     (let [updated-count (sock-count (:variety pair) 1)]
+       (alter gnome update-in [:socks] conj updated-count)
+       (alter dryer update-in [:socks] disj pair)
+       (alter dryer update-in [:socks] conj updated-count)
+       (println (:name @gnome) "stole a sock of variety" (:variety pair) "from" (:name @dryer))))))
+(defn similar-socks
+  "Check if two socks are similar based on variety"
+  [target-sock sock-set]
+  (filter #(= (:variety %) (:variety target-sock)) sock-set))
+(def sock-gnome (ref (generate-sock-gnome "Gnorman")))
+(def dryer (ref {:name "LG 1337" :socks (set (map #(sock-count % 2) sock-varieties))}))
+
+(defn -main
+  "main"
+  [& args]
+  (reset! fred {:cuddle-hunger-level 22
+                :percent-deteriorated 2})
+  (add-watch fred :fred-shuffle-speed shuffle-alert)
+  (swap! fred update-in [:percent-deteriorated] + 1)
+  ;;(swap! bobby update-in [:percent-deteriorated] + 200)
+  (println (:socks @dryer))
+  (steal-sock sock-gnome dryer)
+  (println "Gnorman's socks:" (:socks @sock-gnome))
+  (println (similar-socks (first (:socks @sock-gnome)) (:socks @dryer))))
+
